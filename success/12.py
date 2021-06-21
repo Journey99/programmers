701cd0397695e810c828cc497862ede3add671df
Journey <g48993818@gmail.com>
2021-06-21 10:36:50 +0900

    8th

---
+++ success/12.py
@@ -0,0 +1,6 @@
+# 평균 출력
+
+def solution(arr):
+    return sum(arr) / len(arr)
+
+print(solution([1, 2, 3, 4]))
---
+++ success/13.py
@@ -0,0 +1,6 @@
+# 짝수와 홀수
+
+def solution(num):
+    return "even" if num % 2 == 0 else "odd"
+
+print(solution(3))
